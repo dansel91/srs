84e4675a4aaa0a03679d37b93362493de8b0f903
Pascal Zingg <pzingg@fastforward.ch>
2015-09-29 13:22:01 +0200

    modified maven-generated project

--- src/main/java/ch/bfh/innovation/MainApp.java
+++ src/main/java/ch/bfh/innovation/MainApp.java
@@ -17,19 +17,14 @@ public class MainApp extends Application {
     }
 
     public void start(Stage stage) throws Exception {
-
-        log.info("Starting Hello JavaFX and Maven demonstration application");
-
-        String fxmlFile = "/fxml/hello.fxml";
-        log.debug("Loading FXML for main view from: {}", fxmlFile);
+        String fxmlFile = "/fxml/srs.fxml";
         FXMLLoader loader = new FXMLLoader();
-        Parent rootNode = (Parent) loader.load(getClass().getResourceAsStream(fxmlFile));
+        Parent rootNode = loader.load(getClass().getResourceAsStream(fxmlFile));
 
-        log.debug("Showing JFX scene");
         Scene scene = new Scene(rootNode, 400, 200);
         scene.getStylesheets().add("/styles/styles.css");
 
-        stage.setTitle("Hello JavaFX and Maven");
+        stage.setTitle("SRS");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/test/java/ch/bfh/innovation/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package ch.bfh.innovation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloControllerTest {
+
+    @Test
+    public void testName() throws Exception {
+        assertTrue(true);
+    }
+}
